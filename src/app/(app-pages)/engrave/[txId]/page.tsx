c8f67816b1b8cf663af0a618d958fd1fd90e8aa2
Andrzej Sulkowski <andrzej.sulkowski@protonmail.com>
2024-06-15 12:43:59 +0200

    setup dynamic root font-size

--- src/app/(app-pages)/engrave/[txId]/page.tsx
+++ src/app/(app-pages)/engrave/[txId]/page.tsx
@@ -1,26 +1,25 @@
+'use client';
 import React from "react";
 import { classNames } from "@/utils/className";
-import Label from "@/components/1.atoms/Tag/Tag";
 import Background from "@/components/Svgs/bg/1/Background";
+import { usePathname } from "next/navigation";
 
 interface Props {}
 
 function EngravePage({}: Props) {
+    const path = usePathname();
   return (
     <>
       <Background className="!w-full !h-full !absolute top-0 left-0 overflow-hidden !-z-10" />
       <div className={classNames("xl:px-80 lg:px-56 px-10 pt-28 font-manrope")}>
         <div>
           <div className="inline-block font-extrabold text-6xl mb-6 relative">
-            <h3 className="z-10">Eternalize Your Words</h3>
-            <Label className="absolute -top-5 -right-24 w-auto h-8 text-sm -z-10">
-              On the Bitcoin Blockchain
-            </Label>
+            <h3 className="z-10">Transaction Instruction</h3>
           </div>
-          <span className="text-ei-primary-faded block mb-12">
-            Secure, permanent, and unforgettable- engrave your message on the
-            Bitcoin Blockchain
-          </span>
+          <div className="text-ei-primary-faded block mb-12">
+            To proceed with your transaction, please send the required fees to the Bitcoin address below.
+            <div className="font-bold">Make sure to transfer the exact amount specified</div>
+          </div>
         </div>
       </div>
     </>
