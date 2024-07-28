1de7d83cc7be169c5122df45dcfcbeb8b04f9431
Andrzej Sulkowski <andrzej.sulkowski@protonmail.com>
2024-07-28 11:52:42 +0200

    fix infinite loop and enable mock api

--- src/app/(app-pages)/home/(cmp)/LoadedTicks.tsx
+++ src/app/(app-pages)/home/(cmp)/LoadedTicks.tsx
@@ -6,36 +6,37 @@ import { useBanner } from "@/components/1.atoms/Banner/BannerContext";
 import { useRouter } from "next/navigation";
 import { EIProps } from "@/types";
 import { classNames } from "@/utils/className";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+const tickClick = (id: string, router: AppRouterInstance) =>
+  router.push(`/retrieve/${id}`);
 
 function LoadedTicks({ className }: EIProps) {
   const { showBanner } = useBanner();
   const [ticks, setTicks] = useState<TickProps[]>([]);
-
   const router = useRouter();
-  const tickClick = useCallback(
-    () => (id: string) => router.push(`/retrieve/${id}`),
-    [router]
-  );
+
   const init = useCallback(async () => {
-    const resp = await api.getMessages({ items: 10, after_uuid: null });
-    if (resp.ok) {
-      const ticks: TickProps[] = [];
-      resp.data!.messages.forEach((m) => {
-        ticks.push({
+    try {
+      const resp = await api.getMessages({ items: 10, after_uuid: null });
+      if (resp.ok) {
+        const ticks: TickProps[] = resp.data!.messages.map((m) => ({
           address: m.id,
           txHash: m.tx_id,
-          onClick: tickClick,
-        });
-      });
-      setTicks(ticks);
-    } else {
-      showBanner("error: Failed to load ticks");
+          onClick: () => tickClick(m.id, router),
+        }));
+        setTicks(ticks);
+      } else {
+        showBanner("error: Failed to load ticks");
+      }
+    } catch (error) {
+      showBanner(`error failed to load tickets`);
     }
-  }, [showBanner, tickClick]);
+  }, [showBanner]);
 
   useEffect(() => {
     init();
-  }, [init]);
+  }, []);
 
   return (
     <div className={classNames(className)}>
@@ -43,4 +44,5 @@ function LoadedTicks({ className }: EIProps) {
     </div>
   );
 }
+
 export default LoadedTicks;
